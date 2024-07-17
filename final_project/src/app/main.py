d0158322387cba177d10b40f709cdeff653bac31
studiojessy <dd_jessy@naver.com>
2024-07-17 14:09:41 +0900

    init

---
+++ final_project/src/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import models, database
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if item is None:
+        return {"error": "Item not found"}
+    return item
